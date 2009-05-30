c31176b2ad2c88b2d002958ad0e5ea7aeca4b125
Alan Gutierrez <alan@blogometer.com>
2009-05-30 11:03:13 -0400

    - Using Guice 2.0 from release form some repository somewhere. - Using the new signature for the ObjectFactory that passes an Infusion instead   of an InfusionBuilder.

--- src/main/java/com/goodworkalan/infuse/guice/GuiceFactory.java
+++ src/main/java/com/goodworkalan/infuse/guice/GuiceFactory.java
@@ -3,9 +3,10 @@ package com.goodworkalan.infuse.guice;
 import java.lang.reflect.Type;
 
 import com.goodworkalan.infuse.FactoryException;
-import com.goodworkalan.infuse.InfusionBuilder;
+import com.goodworkalan.infuse.Infusion;
 import com.goodworkalan.infuse.ObjectFactory;
 import com.goodworkalan.infuse.Path;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 
@@ -13,13 +14,13 @@ public class GuiceFactory implements ObjectFactory
 {
     private final Injector injector;
     
+    @Inject
     public GuiceFactory(Injector injector)
     {
         this.injector = injector;
     }
 
-    public Object create(InfusionBuilder builder, Type type, Path context)
-            throws FactoryException
+    public Object create(Infusion infusion, Type type, Path context) throws FactoryException
     {
         return injector.getInstance(Key.get(type));
     }
