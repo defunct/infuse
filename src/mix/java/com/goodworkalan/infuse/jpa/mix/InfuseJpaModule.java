3718cb69e72cfc973e2372997ff3cb6bc2637d27
Alan Gutierrez <alan@blogometer.com>
2009-10-15 11:39:13 -0500

    Update Mix project dependencies.

--- src/mix/java/com/goodworkalan/infuse/jpa/mix/InfuseJpaModule.java
+++ src/mix/java/com/goodworkalan/infuse/jpa/mix/InfuseJpaModule.java
@@ -9,8 +9,6 @@ public class InfuseJpaModule extends BasicJavaModule {
         addDependency(new Artifact("com.goodworkalan", "infuse", "0.1"));
         addDependency(new Artifact("org.hibernate", "hibernate-core", "3.3.1.GA"));
         addDependency(new Artifact("org.hibernate", "hibernate-annotations", "3.4.0.GA"));
-        addTestDependency(new Artifact("org.slf4j", "slf4j-log4j12", "1.4.2"));
-        addTestDependency(new Artifact("log4j", "log4j", "1.2.14"));
         addTestDependency(new Artifact("org.testng", "testng", "5.10"));
         addTestDependency(new Artifact("org.mockito", "mockito-core", "1.6"));
     }
