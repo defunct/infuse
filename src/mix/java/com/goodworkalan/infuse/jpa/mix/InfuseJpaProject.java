d6922c8650bfa16320f03231a35548622e6c74b5
Alan Gutierrez <alan@blogometer.com>
2009-10-27 16:55:48 -0500

    Updated Mix build specification.

---
+++ src/mix/java/com/goodworkalan/infuse/jpa/mix/InfuseJpaProject.java
@@ -0,0 +1,30 @@
+package com.goodworkalan.go.go.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.builder.JavaProject;
+
+public class InfuseJpaProject extends ProjectModule {
+    @Override
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces(new Artifact("com.goodworkalan/infuse-jpa/0.1"))
+                .main()
+                    .depends()
+                        .artifact(new Artifact("com.goodworkalan/infuse/0.1"))
+                        .artifact(new Artifact("org.hibernate/hibernate-core/3.3.1.GA"))
+                        .artifact(new Artifact("org.hibernate/hibernate-annotations/3.4.0.GA"))
+                        .end()
+                    .end()
+                .test()
+                    .depends()
+                        .artifact(new Artifact("org.testng/testng/5.10"))
+                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .end()
+                    .end()
+                .end()
+            .end();
+    }
+}
