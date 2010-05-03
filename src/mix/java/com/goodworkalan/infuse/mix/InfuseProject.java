a525ff98a6cd92327d2b5cede7d0cbf13439e517
Alan Gutierrez <alan@blogometer.com>
2010-05-02 19:51:29 -0500

    Updated for the new Mix build format.

--- src/mix/java/com/goodworkalan/infuse/mix/InfuseProject.java
+++ src/mix/java/com/goodworkalan/infuse/mix/InfuseProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.infuse.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,19 +9,19 @@ public class InfuseProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/infuse/0.1"))
+                .produces("com.github.bigeasy.infuse/infuse/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.github.bigeasy.danger/danger/0.1"))
-                        .artifact(new Artifact("com.github.bigeasy.class-boxer/class-boxer/0.1"))
-                        .artifact(new Artifact("com.github.bigeasy.class-association/class-association/0.1"))
-                        .artifact(new Artifact("com.goodworkalan/reflective/0.1"))
+                        .include("com.github.bigeasy.danger/danger/0.1")
+                        .include("com.github.bigeasy.class-boxer/class-boxer/0.1")
+                        .include("com.github.bigeasy.class-association/class-association/0.1")
+                        .include("com.github.bigeasy.reflective/reflective/0.1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .include("org.testng/testng/5.10/jdk15")
+                        .include("org.mockito/mockito-core/1.6")
                         .end()
                     .end()
                 .end()
