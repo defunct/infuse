2500dbbe3081dd18614bb2afe6533f0638238a99
Alan Gutierrez <alan@blogometer.com>
2010-06-17 15:50:40 -0500

    Changed dependencies for class project refactor.

--- src/mix/java/com/goodworkalan/infuse/mix/InfuseProject.java
+++ src/mix/java/com/goodworkalan/infuse/mix/InfuseProject.java
@@ -19,11 +19,11 @@ public class InfuseProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.infuse/infuse/0.1.1.2")
+                .produces("com.github.bigeasy.infuse/infuse/0.1.1.3")
                 .depends()
                     .production("com.github.bigeasy.danger/danger/0.+1")
-                    .production("com.github.bigeasy.class-boxer/class-boxer/0.+1")
-                    .production("com.github.bigeasy.class-association/class-association/0.+1")
+                    .production("com.github.bigeasy.class/class-boxer/0.+1")
+                    .production("com.github.bigeasy.class/class-association/0.+1")
                     .production("com.github.bigeasy.reflective/reflective-exception/0.+5")
                     .development("org.testng/testng-jdk15/5.10")
                     .development("org.mockito/mockito-core/1.6")
