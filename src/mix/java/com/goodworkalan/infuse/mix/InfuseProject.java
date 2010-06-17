d2cef3008d280af891c588ee487002f953e2093e
Alan Gutierrez <alan@blogometer.com>
2010-06-17 13:02:20 -0500

    Refactored for upstream changes to Verbiage.

--- src/mix/java/com/goodworkalan/infuse/mix/InfuseProject.java
+++ src/mix/java/com/goodworkalan/infuse/mix/InfuseProject.java
@@ -19,7 +19,7 @@ public class InfuseProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.infuse/infuse/0.1.1.1")
+                .produces("com.github.bigeasy.infuse/infuse/0.1.1.2")
                 .depends()
                     .production("com.github.bigeasy.danger/danger/0.+1")
                     .production("com.github.bigeasy.class-boxer/class-boxer/0.+1")
